31169bca39f8bbe5ff7b7129262f87764a3325af
Kshitiz Handa <handakshitiz1092@gmail.com>
2021-06-15 09:23:25 +0530

    commit

--- Start with Terraform Basic/createinstance.tf
+++ Start with Terraform Basic/createinstance.tf
@@ -3,7 +3,7 @@ provider "aws" {
     secret_key = "jKFWXHoOdkjl1Jn93RE4bXwhCyB0kFd+dv38srqY"
     region     = "us-east-2"
 }
-resource "aws_instance" "firstmachine" {
+resource "aws_instance" "first instance" {
   ami           = "ami-05d72852800cbf29e"
   instance_type = "t2.micro"
 }
