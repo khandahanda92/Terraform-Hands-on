f3c3b92361b5a5e73560e2ebcb4ae23c1a654379
Kshitiz Handa <handakshitiz1092@gmail.com>
2021-06-15 20:27:19 +0530

    corrections

--- Terraform Variables/createinstance.tf
+++ Terraform Variables/createinstance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "firstmachine" {
-  ami           = "lookup(var.AMIS, var.AWS_REGION)"
+  ami           = lookup(var.AMIS, var.AWS_REGION)
 
   ## Once the code reach at this location lookup will search the value for aws region inside 
   ## The varibale aws amis , if the value is us east 2 it will search for the ami value from the variable file 
