e3b25f04005da862b0553fdbc4038942e496029a
Kshitiz Handa <handakshitiz1092@gmail.com>
2021-06-15 18:37:20 +0530

    adding few changes

--- Start with Terraform Basic/createinstance.tf
+++ Start with Terraform Basic/createinstance.tf
@@ -1,4 +1,9 @@
 resource "aws_instance" "firstmachine" {
+  count         = 3 
   ami           = "ami-00399ec92321828f5"
   instance_type = "t2.micro"
+
+  tags{
+    Name = "threedemoinstances"
+  }
 }
