c21853756d813c7078e83c0ab7bdc90381ba7784
Kshitiz Handa <handakshitiz1092@gmail.com>
2021-06-15 09:59:51 +0530

    3rd change

--- Start with Terraform Basic/createinstance.tf
+++ Start with Terraform Basic/createinstance.tf
@@ -1,8 +1,3 @@
-provider "aws" {
-    access_key = ""
-    secret_key = ""
-    region     = "us-east-2"
-}
 resource "aws_instance" "firstmachine" {
   ami           = "ami-00399ec92321828f5"
   instance_type = "t2.micro"
