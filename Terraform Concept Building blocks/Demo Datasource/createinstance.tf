94a2684a86431cd74720dac693ecd84895386a28
Kshitiz Handa <handakshitiz1092@gmail.com>
2021-06-17 19:49:26 +0530

    changes

--- Terraform Concept Building blocks/Demo Datasource/createinstance.tf
+++ Terraform Concept Building blocks/Demo Datasource/createinstance.tf
@@ -1,8 +1,27 @@
 data "aws_availability_zones" "available" {}
+
 ## This will fetch all the availablity zones which are available in AWS
 
+
+data "aws_ami" "latest_ubuntu" {  ## To choose AMi 
+  most_recent = true
+  owners = ["099720109477"]
+
+  ## owner for ubuntu you can find on aws finding a linux ami doc
+
+  filter {
+    name = "name"
+    values = "ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 resource "aws_instance" "MyFirstInstnace" {
-  ami           = lookup(var.AMIS, var.AWS_REGION)
+  ami           = data.aws_ami.latest_ubuntu.id # Defining the AMI 
   instance_type = "t2.micro"
   availability_zone = data.aws_availability_zones.available.names[0]
 
