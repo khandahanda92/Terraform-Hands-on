d1b969916b0e9e80dcd5b287b0f40eb34bc5435c
Kshitiz Handa <handakshitiz1092@gmail.com>
2021-06-17 21:03:39 +0530

    commiting changes

--- Terraform Concept Building blocks/Demo Datasource/securitygroup.tf
+++ Terraform Concept Building blocks/Demo Datasource/securitygroup.tf
@@ -1,24 +1,21 @@
-## Create security group on the basis of region filter 
-data "aws_ip_ranges" "us_east_ip_range" { # aws_ip_range is variable 
-regions = ["us-east-1","us-east-2"] # definning on which region we want the ip filter
-services = ["ec2"] # on which services we want this ip filter 
-
-# Datasource for aws_ip_ranges for us_east1 & 2 
-
+data "aws_ip_ranges" "us_east_ip_range" {
+    regions = ["us-east-1","us-east-2"]
+    services = ["ec2"]
 }
 
-resource "aws_security_group" "sg-custom" { # security group name 
-  name  = "sg-custom"
-    
-    ingress {
-      from_port = "443" # From which port you want to accept the traffic 
-      to_port = "443" # to send on which port 
-      protocol = "tcp"
-      cidr_blocks = data.aws_ip_ranges.us_east_ip_range.cidr_blocks
-  }
+resource "aws_security_group" "sg-custom_us_east" {
+    name = "custom_us_east"
 
-   tags = {
-        CreateDate = data.aws_ip_ranges.us_east_ip_range.create_date # date for the security group 
+    ingress {
+
+        from_port = "443"
+        to_port = "443"
+        protocol = "tcp"
+        cidr_blocks = slice(data.aws_ip_ranges.us_east_ip_range.cidr_blocks, 0, 50)
+    }
+
+    tags = {
+        CreateDate = data.aws_ip_ranges.us_east_ip_range.create_date
         SyncToken = data.aws_ip_ranges.us_east_ip_range.sync_token
     }
 }
