83967a67e78126e0c7279fc10c260210edbeeea1
Kshitiz Handa <handakshitiz1092@gmail.com>
2021-06-17 19:37:56 +0530

    changes

--- Terraform Concept Building blocks/Demo Datasource/createinstance.tf
+++ Terraform Concept Building blocks/Demo Datasource/createinstance.tf
@@ -4,8 +4,8 @@ data "aws_availability_zones" "available" {}
 resource "aws_instance" "MyFirstInstnace" {
   ami           = lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
-  availability_zone = data.aws_availability_zones.available.names[1]
-  
+  availability_zone = data.aws_availability_zones.available.names[0]
+
   # This will fetch the availablity zone and put in this variable -available
   # [1] - aws have four availablity zones a, b, c and d - it will choose for index 1 
 
