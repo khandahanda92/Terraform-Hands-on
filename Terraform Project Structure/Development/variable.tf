1b73aadc4717a04e3df537b8584bb742287a3de0
Kshitiz Handa <handakshitiz1092@gmail.com>
2021-06-23 21:10:32 +0530

    sync

---
+++ Terraform Project Structure/Development/variable.tf
@@ -0,0 +1,11 @@
+# This is keep variable to main.tf file 
+
+variable "AWS_REGION" {
+    type    = string
+    default = "us-west-2"
+}
+
+variable "Env" {
+    type    = string
+    default = "development"
+}
---
+++ Terraform Project Structure/Production/variable.tf
@@ -0,0 +1,11 @@
+# This is keep variable to main.tf file 
+
+variable "AWS_REGION" {
+    type    = string
+    default = "us-west-1"
+}
+
+variable "Env" {
+    type    = string
+    default = "Production"
+}
